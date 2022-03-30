c3f1a0c3821cd8632429af9dc731b8416298c714
NaanAvelinoCardoso <naanavelino2018@gmail.com>
2022-03-30 15:32:17 -0300

    base do projeto criado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # nodets-canil
+
+### Pré-requesitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto 
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+// Importando bibliotecas
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRouters from './routers/index';
+
+// Definindo o DOTENV
+dotenv.config();
+
+// Definindo o servidor
+const server = express();
+
+// Configurando o servidor e o template engine e definindo suas pastas
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+// Rotas
+server.use(mainRouters);
+
+server.use((req, res) => {
+    res.send("Pagina não encontrada!!");
+});
+
+
+// Porta do servidor
+server.listen(process.env.PORT);
